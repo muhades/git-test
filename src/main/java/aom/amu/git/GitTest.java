b9551a2b12c0b65df982e233b2a35f0d828a7dd3
muhades <96218263+muhades@users.noreply.github.com>
2022-02-21 15:10:34 +0800

    pull commit

--- src/main/java/aom/amu/git/GitTest.java
+++ src/main/java/aom/amu/git/GitTest.java
@@ -25,5 +25,7 @@ public class GitTest {
 
             System.out.println("hot-fix test1!");
             System.out.println("push test!");
+            
+            System.out.println("pull test!");
       }
 }

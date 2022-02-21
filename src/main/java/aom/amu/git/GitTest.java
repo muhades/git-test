1000f3e89a3f03f802ca14177740e4f9f53dca77
aMu <2976780297@qq.com>
2022-02-21 12:35:57 +0800

    Idea first commit

---
+++ src/main/java/aom/amu/git/GitTest.java
@@ -0,0 +1,15 @@
+package aom.amu.git;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * @author 忧郁的红烧鱼
+ * @create 2022-02-21 12:32
+ */
+public class GitTest {
+      public static void main(String[] args) {
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+            System.out.println("hello git!");
+      }
+}
